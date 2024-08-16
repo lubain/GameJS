7044efb9f0a229821863099c3419b05786218f33
fadhel <fadhelubain@gmail.com>
2024-08-16 22:12:37 +0300

    first commit

---
+++ script/component.js
@@ -0,0 +1,98 @@
+class component {
+    constructor(width, height, color, x, y, type) {
+        this.type = type;
+        if (type == "image") {
+            this.image = new Image();
+            this.image.src = color;
+        }
+        this.color = color;
+        this.score = 0;
+        this.width = width;
+        this.height = height;
+        this.speedX = 0;
+        this.speedY = 0;    
+        this.x = x;
+        this.y = y; 
+        this.gravity = 0;
+        this.gravitySpeed = 0;
+        this.direction = DIRECTION_UP;
+        this.nextDirection = this.direction;
+        this.currentFrame = 1;
+        this.frameCount = 7;
+    }
+
+    update() {
+        let context = zoneDuJeu.context;
+        if (this.type == "image") {
+            context.drawImage(
+                this.image, 
+                this.x, 
+                this.y,
+                this.width,
+                this.height
+            );
+        } else {
+            context.fillStyle = this.color;
+            context.fillRect(this.x, this.y, this.width, this.height);
+        }
+        if (this.type == "text") {
+            context.font = this.width + " " + this.height;
+            context.fillStyle = this.color;
+            context.fillText(this.text, this.x, this.y);
+        }
+    }
+
+    newPos() {
+        this.gravitySpeed += this.gravity;
+        this.x += this.speedX;
+        this.y += this.speedY + this.gravitySpeed;
+        this.hitBottom();
+    }
+    
+    hitBottom() {
+        var surLeSol = canvas.height - this.height;
+        if (this.y > surLeSol) {
+            this.y = surLeSol;
+            this.gravitySpeed = 0;
+        } else if(this.y<0) {
+            this.y = 0;
+            this.gravitySpeed = 0;
+        }
+    }
+    
+    crashWith(otherobj) {
+        var myleft = this.x;
+        var myright = this.x + (this.width);
+        var mytop = this.y;
+        var mybottom = this.y + (this.height);
+        var otherleft = otherobj.x;
+        var otherright = otherobj.x + (otherobj.width);
+        var othertop = otherobj.y;
+        var otherbottom = otherobj.y + (otherobj.height);
+        var crash = true;
+        if ((mybottom < othertop) || (mytop > otherbottom) || (myright < otherleft) || (myleft > otherright)) {
+            crash = false;
+        }
+        return crash;
+    }
+
+    eat() {
+        
+    }
+
+    draw() {
+        canvasContext.save();
+        canvasContext.translate(
+            this.x + oneBlockSize / 2,
+            this.y + oneBlockSize / 2
+        );
+        canvasContext.rotate((this.direction * 90 * Math.PI) / 180)
+        
+        canvasContext.translate(
+            -this.x - oneBlockSize / 2,
+            -this.y - oneBlockSize / 2
+        );
+
+        canvasContext.restore();
+    }
+}
---
+++ script/game.js
@@ -0,0 +1,283 @@
+const canvas = document.getElementById("canvas");
+const canvasContext = canvas.getContext("2d");
+
+const EmojyFrames = document.getElementById("animations");
+
+let Emojy;
+let val = 12;
+let tire = false;
+let pause = true;
+let weap;
+let bareDeVie;
+
+let createRect = (x, y, width, heigth, color)=>{
+    canvasContext.fillStyle = color;
+    canvasContext.fillRect(x, y, width, heigth);
+}
+
+let fps = 30;
+let oneBlockSize = 30;
+let score = 0;
+
+const DIRECTION_RIGHT = 4
+const DIRECTION_UP = 3
+const DIRECTION_LEFT = 2
+const DIRECTION_BOTTOM = 1
+
+let Obstacles = [];
+let Sound;
+let SoundHitObstacle;
+let Score;
+let munitionText;
+let munition = 3;
+
+
+function startGame() {
+    createNewEmojy();
+    Emojy.gravity = 0.05;
+    Score = new component("30px", "Consolas", "blue", 20, 50, "text");
+    munitionText = new component("20px", "Consolas", "rgb(221, 42, 42)", 20, 80, "text");
+    bareDeVie = new component(30, 5, "green", Emojy.x, Emojy.y - 5);
+    Sound = new sound("./assets/sound/bounce.mp3");
+    SoundHitObstacle = new sound("./assets/sound/eatpill.mp3");
+    zoneDuJeu.start();
+}
+
+let zoneDuJeu = {
+    start : function() {
+        canvas.width = window.innerWidth;
+        canvas.height = window.innerHeight/2;
+        this.context = canvasContext;
+        this.frameNo = 0;
+        this.interval = setInterval(updateGameArea, val);
+    },
+    clear : function() {
+        this.context.clearRect(0, 0, canvas.width, canvas.height);
+    },
+    stop : function() {
+        clearInterval(this.interval);
+    },
+    continues : function() {
+        this.interval = setInterval(updateGameArea, val);
+    }
+}
+
+let container = document.querySelector(".container");
+
+let a = 200;
+
+let currentFrame = 0;
+
+function updateGameArea() {
+    var x, height, gap, minHeight, maxHeight, minGap, maxGap;
+    for (i = 0; i < Obstacles.length; i += 1) {
+        if (Emojy.crashWith(Obstacles[i])) {
+            bareDeVie.width -= 10;
+            if (bareDeVie.width <= 0) {
+                Sound.play();
+                zoneDuJeu.stop();
+                container.style.display = "block";
+                return;
+            } else if (bareDeVie.width <= 10) {
+                bareDeVie.color = "red";
+            } else if (bareDeVie.width <= 20) {
+                bareDeVie.color = "rgb(255, 166, 0)";
+            }
+            SoundHitObstacle.play();
+            Obstacles[i] = Obstacles[Obstacles.length-1];
+            Obstacles.pop();
+        }
+    }
+    zoneDuJeu.clear();
+    zoneDuJeu.frameNo += 1;
+    currentFrame++;
+
+    if ((zoneDuJeu.frameNo/12) % 100 == 0) {
+        munition++;
+    }
+
+    if ((zoneDuJeu.frameNo/12 >= 100)&&(zoneDuJeu.frameNo/12 < 300)){
+        a = 225;
+    } else if (zoneDuJeu.frameNo/12 >= 500) {
+        a = 250;
+    }
+
+    if (zoneDuJeu.frameNo == 1 || everyinterval(a)) {
+        x = canvas.width;
+        minHeight = 30;
+        maxHeight = 200;
+        height = Math.floor(Math.random()*(maxHeight-minHeight+1)+minHeight);
+        minGap = 50;
+        maxGap = 200;
+        gap = Math.floor(Math.random()*(maxGap-minGap+1)+minGap);
+        Obstacles.push(new component(Math.floor(Math.random()*(30)+10), height, "rgb(0, 158, 250)", x, 0));
+        Obstacles.push(new component(Math.floor(Math.random()*(30)+10), x - height - gap, "rgb(0, 158, 250)", x, height + gap));
+    }
+    for (i = 0; i < Obstacles.length; i += 1) {
+        if (zoneDuJeu.frameNo/12 < 500) {
+            Obstacles[i].x -= (1 + zoneDuJeu.frameNo/1500);
+        } else if (zoneDuJeu.frameNo/12 >= 500) {
+            Obstacles[i].x -= 5;
+        }
+        if (Obstacles[i].width >= 20) {
+            if (currentFrame<=100) {
+                if (Obstacles[i].y == 0) {
+                    Obstacles[i].height += 1.3;
+                } else {
+                    Obstacles[i].y += 1;
+                }
+            } else if (currentFrame <= 200) {
+                if (Obstacles[i].y == 0) {
+                    Obstacles[i].height -= 1.3;
+                } else {
+                    Obstacles[i].y -= 1;
+                }
+            } else {
+                currentFrame = 0;
+            }
+            Obstacles[i].color = "rgb(28, 190, 23)";
+        }
+
+        Obstacles[i].update();
+    }
+
+    if (tire) {
+        weap.update();
+        for (i = 0; i < Obstacles.length; i += 1) {
+            if (weap.crashWith(Obstacles[i])) {
+                SoundHitObstacle.play();
+                Obstacles[i] = Obstacles[Obstacles.length-1];
+                Obstacles.pop();
+                tire = false;
+                break;
+            }
+        }
+        weap.x += 3;
+    }
+
+    bareDeVie.x = Emojy.x;
+    bareDeVie.y = Emojy.y - 10;
+
+    Score.text="SCORE: " + Math.floor(zoneDuJeu.frameNo/12);
+    Score.update();
+    munitionText.text="Munition: " + munition;
+    munitionText.update();
+    Emojy.newPos();
+    Emojy.update();
+    Emojy.draw();
+    bareDeVie.update();
+}
+
+let drawFoods = () => {
+    
+}
+
+function destroy(obj) {
+    SoundHitObstacle.play();
+    obj = Obstacles[Obstacles.length-1];
+    Obstacles.pop();
+}
+
+let createNewEmojy = () => {
+    Emojy = new component(
+        oneBlockSize,
+        oneBlockSize,
+        "./assets/gif/smiley.gif",
+        100,
+        120,
+        "image"
+    );
+};
+
+let restart = ()=> {
+    container.style.display = "none";
+    Obstacles = [];
+    zoneDuJeu.stop();
+    tire = false;
+    munition = 3;
+    startGame();
+}
+
+window.addEventListener("keydown", (event) => {
+    let k = event.keyCode;
+    let option = document.querySelector(".option");
+    setTimeout(() =>{
+        if (k == 37 || k == 81) {
+            move();
+            moveleft();
+        } else if ((k == 38 || k == 90)&&(pause)) {
+            Sound.play();
+            accelerate(-0.2,true);
+        } else if ( k == 39 || k == 68) {
+            move();
+            moveright();
+        } else if (k == 27) {
+            if (pause) {
+                option.style.display = "block";
+                pause = false;
+                zoneDuJeu.stop();
+            } else {
+                option.style.display = "none";
+                pause = true;
+                zoneDuJeu.continues();
+            }
+
+        } else if ((k == 70)&&(pause)) {
+            if (munition > 0) {
+                weap = new component(20, 2, "red", Emojy.x+20, Emojy.y+5);
+                tire = true;
+                munition--;
+            }
+        }
+    }, 1)
+})
+window.addEventListener("keyup", (event) => {
+    let k = event.keyCode
+
+    setTimeout(() =>{
+        if (k == 38 || k == 90) {
+            accelerate(0.05,false);
+        }
+        clearmove();
+        idlMove();
+    }, 1)
+});
+
+let ctx = function() {
+    this.canvasContext.clearRect(0, 0, this.canvas.width, this.canvas.height);
+}
+
+function move() {
+    Emojy.image.src = "./assets/gif/angry.gif";
+}
+
+function clearmove() {
+    Emojy.image.src = "./assets/gif/smiley.gif";
+}
+
+function everyinterval(n) {
+    if ((zoneDuJeu.frameNo / n) % 1 == 0) {return true;}
+    return false;
+}
+
+function moveleft() {
+    Emojy.speedX = -1; 
+}
+
+function moveright() {
+    Emojy.speedX = 1; 
+}
+
+function idlMove() {
+    Emojy.speedX = 0;
+}
+
+function accelerate(n,k) {
+    if (k){
+        move();
+    }else{
+        clearmove();
+    }
+    Emojy.gravity = n;
+    i++;
+}
